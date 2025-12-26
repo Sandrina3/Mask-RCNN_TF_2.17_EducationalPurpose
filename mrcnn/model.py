293200812b7964f2dbb77d554cc390ded817fa60
Sandrina3 <120737095+Sandrina3@users.noreply.github.com>
2025-12-26 21:36:17 +0100

    Update model.py

--- mrcnn/model.py
+++ mrcnn/model.py
@@ -2280,6 +2280,8 @@ class MaskRCNN(object):
 
             tf.print(" | ".join([f"{k}: {v}" for k, v in losses.items()]))
 
+		print("Number of trainable variables:", len(self.keras_model.trainable_variables))
+
         grads = tape.gradient(total_loss, self.keras_model.trainable_variables)
 
         # for g, v in zip(grads, self.keras_model.trainable_variables):

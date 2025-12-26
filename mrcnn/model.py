4aebdd7c7234414db01b74b360282ab5ce0e98a2
Sandrina3 <120737095+Sandrina3@users.noreply.github.com>
2025-12-26 22:08:04 +0100

    Update model.py

--- mrcnn/model.py
+++ mrcnn/model.py
@@ -2376,7 +2376,7 @@ class MaskRCNN(object):
             if not layer.weights:
                 continue
             # Is it trainable?
-            trainable = bool(re.search(layer_regex, layer.name))
+            trainable = bool(re.fullmatch(layer_regex, layer.name))
             # Update layer. If layer is a container, update inner layer.
             if layer.__class__.__name__ == 'TimeDistributed':
                 layer.layer.trainable = trainable
@@ -2461,6 +2461,20 @@ class MaskRCNN(object):
             defined in the Dataset class.
         """
         assert self.mode == "training", "Create model in training mode."
+
+        # Pre-defined layer regular expressions
+        layer_regex = {
+            # all layers but the backbone
+            "heads": r"(mrcnn\_.*)|(rpn\_.*)|(fpn\_.*)",
+            # From a specific Resnet stage and up
+            "3+": r"(res3.*)|(bn3.*)|(res4.*)|(bn4.*)|(res5.*)|(bn5.*)|(mrcnn\_.*)|(rpn\_.*)|(fpn\_.*)",
+            "4+": r"(res4.*)|(bn4.*)|(res5.*)|(bn5.*)|(mrcnn\_.*)|(rpn\_.*)|(fpn\_.*)",
+            "5+": r"(res5.*)|(bn5.*)|(mrcnn\_.*)|(rpn\_.*)|(fpn\_.*)",
+            # All layers
+            "all": ".*",
+        }
+        if layers in layer_regex.keys():
+            layers = layer_regex[layers]
 
         # Data generators
         train_generator = DataGenerator(train_dataset, self.config, shuffle=True,

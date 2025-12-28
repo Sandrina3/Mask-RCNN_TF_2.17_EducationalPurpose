9997607694ae28c7e18bde506862e7f68908debf
Sandrina3 <120737095+Sandrina3@users.noreply.github.com>
2025-12-28 16:32:07 +0100

    Update GPU settings and image dimensions
    Increased GPU image processing capacity and adjusted image dimensions.

--- mrcnn/cell.py
+++ mrcnn/cell.py
@@ -137,7 +137,7 @@ class CellConfig(Config):
     NAME = "cell"
 
     # Adjust depending on your GPU memory
-    IMAGES_PER_GPU = 1
+    IMAGES_PER_GPU = 8
 
     # Number of classes (including background)
 
@@ -161,8 +161,8 @@ class CellConfig(Config):
     # Input image resizing
     # Random crops of size 512x512
     IMAGE_RESIZE_MODE = "crop"
-    IMAGE_MIN_DIM = 256#256#64#128#256#512
-    IMAGE_MAX_DIM = 256#256#64#128#256#512
+    IMAGE_MIN_DIM = 512#256#64#128#256#512
+    IMAGE_MAX_DIM = 512#256#64#128#256#512
     IMAGE_MIN_SCALE = 2.0#1.0
 
     # Length of square anchor side in pixels
